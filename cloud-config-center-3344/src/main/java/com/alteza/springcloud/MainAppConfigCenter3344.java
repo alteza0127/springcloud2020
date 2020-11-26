df26e7d92ad05c2ce0b1972bbaf524717b8d6ca5
Alteza <923960128@qq.com>
2020-11-26 14:37:35 +0800

    springcloud config配置 bus 动态刷新（RabbitMQ 全局广播）

---
+++ cloud-config-center-3344/src/main/java/com/alteza/springcloud/MainAppConfigCenter3344.java
@@ -0,0 +1,18 @@
+package com.alteza.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author Alteza
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class MainAppConfigCenter3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainAppConfigCenter3344.class, args);
+    }
+
+}
---
+++ cloud-config-client-3366/src/main/java/com/alteza/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,27 @@
+package com.alteza.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Alteza
+ * @RefreshScope 动态刷新
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config-info")
+    public String getConfigInfo() {
+        return configInfo + "***" + serverPort;
+    }
+
+}
